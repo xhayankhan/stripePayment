2200a22b5d0ee56660b5c71e4a94c0f9b2cbd853
xhayankhan <87603317+xhayankhan@users.noreply.github.com>
2023-03-13 15:46:40 +0500

    Update createCharge.route.js

--- routes/createChargeRoute/createCharge.route.js
+++ routes/createChargeRoute/createCharge.route.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const httpPostCreateChargeHandler = require("../../controller/getCustomerCardsController/getCustomerCards.controller");
+const httpPostCreateChargeHandler = require("../../controller/createChargeController/createCharge.controller");
 router.post("/", httpPostCreateChargeHandler);
 module.exports = router;
